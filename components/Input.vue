05c17adcbb9478be324bb17d2bd1a7bbfdeb40e9
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-11-01 18:51:56 +0300

    2.3 Добавил сброс фильтров, при отсутствии результатов. Добавил сброс поля с поиском.

--- components/Input.vue
+++ components/Input.vue
@@ -1,31 +1,49 @@
 <template>
+  <div class="input-container">
     <input type="text" :value="modelValue" @input="onInput" :placeholder="placeholder" class="input" />
-  </template>
+    <span class="clear-icon" v-if="modelValue" @click="clearInput">✖</span>
+  </div>
+</template>
   
-  <script lang="ts" setup>
-  import { defineProps, defineEmits } from 'vue';
-  
-  const props = defineProps<{
-    modelValue: string;
-    placeholder: string;
-  }>();
-  
-  const emit = defineEmits(['update:modelValue']);
-  
-  function onInput(event: Event) {
-    const target = event.target as HTMLInputElement;
-    emit('update:modelValue', target.value);
-  }
-  </script>
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+const searchQuery = ref('');
+const props = defineProps<{
+  modelValue: string;
+  placeholder: string;
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+
+function onInput(event: Event) {
+  const target = event.target as HTMLInputElement;
+  searchQuery.value = target.value;
+  emit('update:modelValue', target.value);
+}
+
+function clearInput() {
+  searchQuery.value = '';
+  emit("update:modelValue", searchQuery.value);
+}
+</script>
 
 <style lang="scss" scoped>
+.input-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 300px;
+}
+
 .input {
   font-family: "PT Sans Caption", sans-serif;
   display: block;
   width: 269px;
   padding: 15px 14.5px;
   font-size: 1rem;
-  border: 1px solid #ddd;
+  border: 1px solid #00000015;
   outline: none;
   transition: border-color 0.3s, box-shadow 0.3s;
   color: #aaa;
@@ -40,5 +58,21 @@
 .input::placeholder {
   color: #aaa;
   font-family: "PT Sans Caption", sans-serif;
+}
+
+.clear-icon {
+  position: absolute;
+  right: 15px;
+  background: none;
+  border: none;
+  font-size: 1.2rem;
+  color: #ddd;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+}
+
+.clear-icon:hover {
+  color: #ff0000; /* Цвет крестика при наведении */
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,14 +12,15 @@
 
         <Input v-model="searchQuery" placeholder="Поиск экскурсии" />
       </div>
-       
-      <div class="tour-grid">
-        <TourCard v-for="tour in filteredTours" :key="tour.id" :tour="tour" />
+      <div class="tours-container">
+        <div v-if="filteredTours.length === 0 && selectedCity && !loadingTours" class="no-tours">
+          <p class='no-tours-text'>Поиск не дал результатов</p>
+          <button @click="resetFilters" class='no-tours-button'>Сбросить фильтры</button>
+        </div>
+        <div v-else class="tour-grid">
+          <TourCard v-for="tour in filteredTours" :key="tour.id" :tour="tour" />
+        </div>
       </div>
-
-      <a href='/api/cities?api_key=873fa71c061b0c36d9ad7e47ec3635d9&username=frontend@sputnik8.com'>
-        To endpoint
-      </a>
     </app-layout>
 </template>
 
@@ -37,6 +38,7 @@ const tours = ref<Product[]>([]);
 const selectedCity = ref<number>(0);
 const searchQuery = ref<string>('');
 const loadingCities = ref<boolean>(false);
+const loadingTours = ref<boolean>(false);
 
 const filteredTours = computed(() => {
   return tours.value.filter(tour =>
@@ -61,6 +63,7 @@ async function fetchCities() {
 async function fetchTours() {
 console.log(selectedCity.value);
   if (selectedCity.value) {
+    loadingTours.value = true;
     try {
       const response = await fetch(`/api/products?city_id=${selectedCity.value}&api_key=873fa71c061b0c36d9ad7e47ec3635d9&username=frontend@sputnik8.com`);
       const data = await response.json();
@@ -69,13 +72,50 @@ console.log(selectedCity.value);
     } catch (error) {
       console.error('Ошибка при загрузке экскурсий:', error);
     }
+    finally{
+      loadingTours.value = false;
+    }
   }
+}
+
+// Функция для сброса фильтров
+function resetFilters() {
+  searchQuery.value = '';
+  selectedCity.value = 0;
+  tours.value = [];
 }
 
 onMounted(fetchCities);
 </script>
 
 <style lang="scss" scoped>
+
+    .no-tours {
+        width:50%;
+        padding-top: 5.625rem;
+        margin: auto;
+    }
+
+    .no-tours-text{
+      text-align: center;
+      font-family: "PT Sans Caption", sans-serif;
+      font-size: 1.5rem;
+      line-height: 2.125rem;
+      color: #000;
+    }
+
+    .no-tours-button{
+      display: block;
+      color: #FFF;
+      background: #00A7FF;
+      font-size: 0.875rem;
+      line-height: 2.5rem;
+      padding: 0rem 2.125rem;
+      border-radius: 3px;
+      border: 0;
+      margin: auto;
+    }
+
     .tour-grid {
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(min(350px, 100%), 1fr));
